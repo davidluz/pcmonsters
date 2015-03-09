b2f70fb79c8d8bf1e0a678f7e91d5eb5f537212d
davidluz <davidmelo8@hotmail.com>
2015-03-08 21:05:37 -0300

    Commit1

---
+++ Assets/Scripts/MudarCena.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MudarCena : MonoBehaviour {
+
+	public  void goToScene(int cena){
+		if (cena == 1) {
+						Application.LoadLevel ("Menu");
+				}
+		if (cena == 2) {
+						Application.LoadLevel ("Battle");
+				}
+		if (cena == 3) {
+			Application.LoadLevel ("Training");
+		}
+
+
+	}
+
+
+
+	 
+	// Use this for initialization
+	void Start () {
+
+	
+
+
+	}
+
+
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
