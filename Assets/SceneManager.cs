68dfea4fe181a1777a2e5ed95c77d1f012047b6d
davidluz <davidmelo8@hotmail.com>
2015-03-15 20:30:49 -0300

    V2

---
+++ Assets/SceneManager.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+
+
+public class SceneManager : MonoBehaviour {
+
+	public Text mensagem;
+	public void createMonster(){
+
+	}
+
+	public void mudaTexto(){
+		mensagem.text = "A batalhar terminou";
+	}
+
+
+	public void changeScene(int sceneNumber){
+
+		if (sceneNumber == 1) {
+			Application.LoadLevel ("Scene1");
+				}
+		if (sceneNumber == 2) {
+			Application.LoadLevel ("Scene2");
+		}
+		if (sceneNumber == 3) {
+			Application.LoadLevel ("Scene3");
+		}
+
+		}
+
+}
